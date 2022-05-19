b84c27f6c5e5163443a47ea2424410fae81a5a98
Oscar Otero <oom@oscarotero.com>
2022-05-19 17:15:59 +0200

    fixes, fireworks, fancy stuff

--- _config.ts
+++ _config.ts
@@ -2,9 +2,13 @@ import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import basePath from "lume/plugins/base_path.ts";
 
-export default lume()
+const site = lume();
+
+site
   .copy([".png", ".svg"])
   .copy("scripts")
   .use(basePath())
   .use(postcss())
-  .data("dev", true);
+  .data("dev", site.options.dev);
+
+export default site;
