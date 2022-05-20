8ff338b814791fc8c19a0cabcddec60241421e62
Oscar Otero <oom@oscarotero.com>
2022-05-20 13:52:04 +0200

    setup a cms

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import basePath from "lume/plugins/base_path.ts";
+import cms from "lume/plugins/netlify_cms.ts";
 
 const site = lume();
 
@@ -9,6 +10,10 @@ site
   .copy("scripts")
   .use(basePath())
   .use(postcss())
-  .data("dev", site.options.dev);
+  .use(cms({
+    previewStyle: "styles/admin.css",
+  }))
+  .data("dev", site.options.dev)
+  .data("cache", Date.now());
 
 export default site;
---
+++ _data/netlify_cms.js
@@ -0,0 +1,100 @@
+const config = {
+  backend: {
+    name: "gitlab",
+    repo: "marketgoo/badger-seo/landing",
+    branch: "master",
+    oauth_type: "pkce",
+    app_id: "9037e271942a29264bc8671e55345e0a2cd7c2b5dfd6706a90110b9c20901dc0"
+  },
+  media_folder: "img",
+  collections: [],
+};
+
+// Individual pages
+config.collections.push({
+  label: "Pages",
+  name: "pages",
+  description: "Here you can edit your individual pages",
+  sortable_fields: ["title"],
+  preview: false,
+  files: [
+    {
+      label: "Landing",
+      name: "landing",
+      file: "/index.yml",
+      fields: [
+        field("Title"),
+        field("layout", "hidden"),
+        field("Header", "object", {
+          fields: [
+            field("Title"),
+            field("Intro", "markdown"),
+            field("CTA"),
+          ],
+        }),
+        field("Menu", "list", {
+          fields: [
+            field("Text"),
+            field("Href"),
+          ],
+        }),
+        field("Steps", "list", {
+          fields: [
+            field("Title"),
+            field("Description", "markdown"),
+            field("Class"),
+            field("Img", "image"),
+          ],
+        }),
+        field("Resume", "markdown"),
+        field("Join", "object", {
+          fields: [
+            field("Title"),
+            field("Description", "markdown"),
+            field("Email", "object", {
+              fields: [
+                field("Label"),
+                field("Placeholder"),
+              ],
+            }),
+            field("CTA"),
+            field("Success", "markdown"),
+            field("Error", "markdown"),
+          ],
+        })
+      ],
+    },
+  ],
+});
+
+
+
+function field(label, widget = "string", extra = {}) {
+  const defaults = {};
+
+  if (typeof widget === "object") {
+    extra = widget;
+    widget = "string";
+  }
+
+  if (widget === "list") {
+    defaults.collapsed = true;
+    defaults.minimize_collapsed = true;
+  } else if (widget === "object") {
+    defaults.collapsed = true;
+  } else if (widget === "markdown") {
+    defaults.minimal = true;
+  } else if (widget === "select" || widget === "boolean") {
+    defaults.required = false;
+  }
+
+  return {
+    label,
+    name: label.toLowerCase().replaceAll(" ", "_"),
+    widget,
+    ...defaults,
+    ...extra,
+  };
+}
+
+export default config;
