4ad49ecf28600f604aac0c25dc3ca4ddd8ba0056
Oscar Otero <oom@oscarotero.com>
2022-05-23 16:43:26 +0200

    added instructions

--- _config.ts
+++ _config.ts
@@ -5,6 +5,7 @@ import cms from "lume/plugins/netlify_cms.ts";
 export default lume()
   .copy("static", ".")
   .use(postcss())
+  .ignore("README.md")
   .use(cms({
     previewStyle: "styles/admin.css",
     netlifyIdentity: true,
