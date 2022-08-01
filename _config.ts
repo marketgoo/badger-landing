ae99f0ab33dcd97da8160d386d7455da5656aef6
Oscar Otero <oom@oscarotero.com>
2022-08-01 13:29:39 +0200

    added changelog

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from "lume/mod.ts";
 import postcss from "lume/plugins/postcss.ts";
 import cms from "lume/plugins/netlify_cms.ts";
 import metas from "lume/plugins/metas.ts";
+import date from "lume/plugins/date.ts";
 
 export default lume({
   location: new URL("https://getbadger.io"),
@@ -9,6 +10,7 @@ export default lume({
   .copy("static", ".")
   .ignore("README.md")
   .use(postcss())
+  .use(date())
   .use(metas())
   .use(cms({
     previewStyle: "styles/admin.css",
